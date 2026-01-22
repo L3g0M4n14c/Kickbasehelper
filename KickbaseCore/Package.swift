c9191f914c36b767f2f8a9fc52f28fb67920386b
L3g0M4n14c <L3g0M4n14c@users.noreply.github.com>
2026-01-22 14:08:00 +0100

    fixes

--- KickbaseCore/Package.swift
+++ KickbaseCore/Package.swift
@@ -5,27 +5,27 @@ let package = Package(
     name: "KickbaseCore",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v17), .macOS(.v14)
+        .iOS(.v17), .macOS(.v14),
     ],
     products: [
         .library(
             name: "KickbaseCore",
-            targets: ["KickbaseCore"]),
+            targets: ["KickbaseCore"])
     ],
     dependencies: [
-        .package(url: "https://source.skip.tools/skip.git", from: "1.0.0"),
-        .package(url: "https://source.skip.tools/skip-ui.git", from: "1.0.0"),
-        .package(url: "https://source.skip.tools/skip-foundation.git", from: "1.0.0")
+        .package(url: "https://github.com/skiptools/skip.git", from: "1.0.0"),
+        .package(url: "https://github.com/skiptools/skip-ui.git", from: "1.0.0"),
+        .package(url: "https://github.com/skiptools/skip-foundation.git", from: "1.0.0"),
     ],
     targets: [
         .target(
             name: "KickbaseCore",
             dependencies: [
                 .product(name: "SkipUI", package: "skip-ui"),
-                .product(name: "SkipFoundation", package: "skip-foundation")
+                .product(name: "SkipFoundation", package: "skip-foundation"),
             ],
             plugins: [
                 .plugin(name: "skipstone", package: "skip")
-            ]),
+            ])
     ]
 )
